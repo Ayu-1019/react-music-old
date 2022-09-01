d905fada2228c6476eab64460cd166ce098138e9
zhangxueyu <1501109542@qq.com>
2022-09-01 22:00:33 +0800

    feat: 在axios中header携带token

--- src/services/request.js
+++ src/services/request.js
@@ -1,4 +1,5 @@
 import originAxios from 'axios';
+import { getToken } from '../utils/handle-data';
 
 export default function request(option) {
 	return new Promise((resolve, reject) => {
@@ -15,6 +16,9 @@ export default function request(option) {
 			// 1.当发送网络请求时, 在页面中添加一个loading组件, 作为动画
 
 			// 2.某些请求要求用户必须登录, 判断用户是否有token, 如果没有token跳转到login页面
+      if(getToken()) {
+        config.headers['Authorization'] = 'Bear ' + getToken()
+      }
 
 			// 3.对请求的参数进行序列化(看服务器是否需要序列化)
 			// config.data = qs.stringify(config.data)
